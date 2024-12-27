af957d652e051bae661445e7b9fafef364c70a35
GOOMBANGEE <goom@goomba.org>
2024-12-27 13:29:32 +0900

    fix: validator setting

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   // ejs 설정
@@ -10,6 +11,16 @@ async function bootstrap() {
   app.setViewEngine('ejs');
   // ejs 템플릿 경로 설정
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  // validator 설정
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에서 정의된 속성만 허용
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 속성은 거부
+      transform: true, // 요청된 데이터를 DTO로 변환
+      skipMissingProperties: false, // 필수 항목이 없으면 거부
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
